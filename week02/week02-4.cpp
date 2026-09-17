184ab85a0c051b1fbae6c21a1b6a9f1e8aa01d0a
an020202 <zfuxiang272@gmail.com>
2026-09-17 11:50:15 +0800

    14750473

--- week02/week02-4.cpp
+++ week02/week02-4.cpp
@@ -1,1 +1,16 @@
-
+//week02-4.cpp¾Ç²ß­pµe
+//Leetcode 389. Find the Difference
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int U[26] ={};
+        for(char c : s){
+            U[c - 'a'] ++;
+        }
+        for(char c : t){
+            U[c - 'a'] --;
+            if(U[c-'a'] < 0 )return c;
+        }
+        return 0;
+    }
+};
