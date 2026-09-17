55b4d54a90393ec6e547e7a07182fe014c4abe1b
an020202 <zfuxiang272@gmail.com>
2026-09-17 11:47:38 +0800

    14750473

---
+++ week02/week02-1.cpp
@@ -0,0 +1,13 @@
+/// week02-1.cpp 今天的主題 C++string 字串
+/// File-Save As 要自己輸入副檔名 .cpp
+#include <iostream>
+#include <string>
+/// 現在是字特別多的版本
+int main()
+{
+
+    std::cout << "請問你叫什麼名子啊?";
+    std::string name; ///宣告字串name
+    std:: cin>> name; ///上週教cin原來長這樣
+    std::cout <<name<< "你好,今天教字串喔!";
+}
